7fce33aeeb94d508b1a566a56f9262025e1692f7
Tobias Tobak <tobiasfunction@protonmail.com>
2022-08-11 14:51:54 -0400

    Add preliminary spacing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 function App() {
   function NetworksContainer(props) {
-    return <div className="bg-orange"> Network Chooser</div>;
+    return <div className="bg-orange m-2 p-1"> Network Chooser</div>;
   }
 
   function AllStations(props) {
-    return <div className="bg-gray-light">All Stations</div>;
+    return <div className="bg-gray-light m-2 p-1">All Stations</div>;
   }
 
   function FavoriteStations(props) {
     return (
-      <div className="bg-blue text-white border-2 border-gray-dark">
+      <div className="bg-blue text-white border-2 border-gray-dark m-2 p-1">
         Favorite Stations
       </div>
     );
@@ -17,20 +17,22 @@ function App() {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="bg-blue text-white font-extrabold text-center">Where's A Bike?</header>
-      {/* App body */}
-      <div className="container mx-auto flex-grow md:flex md:flex-row-reverse md:justify-center">
+      <header className="bg-blue text-white font-extrabold text-center p-1">
+        Where's A Bike?
+      </header>
+      {/* app body */}
+      <div className="grow md:flex md:flex-row-reverse md:justify-center p-1">
         {/* right column */}
-        <div>
+        <div className="md:w-64">
           <FavoriteStations />
         </div>
         {/* left column */}
-        <div>
+        <div className="md:w-96">
           <NetworksContainer />
           <AllStations />
         </div>
       </div>
-      <footer className="w-full bg-blue text-white self-end">footer</footer>
+      <footer className="w-full bg-blue text-white self-end p-2">footer</footer>
     </div>
   );
 }
