a95b49a3f763a4e634c4e2d65c9dc80b684be992
Tobias Tobak <tobiasfunction@protonmail.com>
2022-08-11 12:04:38 -0400

    Set theme colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import "./App.css";
-
 function App() {
+  function NetworksContainer(props) {
+    return <div className="bg-orange"> Network Chooser</div>;
+  }
+
+  function AllStations(props) {
+    return <div className="bg-gray-light">All Stations</div>;
+  }
+
+  function FavoriteStations(props) {
+    return (
+      <div className="bg-blue text-white border-2 border-gray-dark">
+        Favorite Stations
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <footer className="App-footer"></footer>
+    <div>
+      <header className="bg-blue text-white">Where's A Bike</header>
+      <div className="container mx-auto">
+        <NetworksContainer />
+        <AllStations />
+        <FavoriteStations />
+      </div>
+      <footer className="bg-blue text-white">footer</footer>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#417D9F",
+        gray: {
+          light: "#F5F5F5",
+          dark: "#C9C9C9",
+        },
+        orange: "#E8AA56",
+      },
+      fontFamily: {
+        sans: ["Inter var"],
+      },
+    },
   },
   plugins: [],
 };
