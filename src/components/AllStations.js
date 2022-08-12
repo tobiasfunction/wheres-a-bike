1d890cb79297a3ef7662ee715aa8ffc7e98aa1f9
Tobias Tobak <tobiasfunction@protonmail.com>
2022-08-11 23:12:51 -0400

    Include station bike/slot counts

--- src/components/AllStations.js
+++ src/components/AllStations.js
@@ -6,25 +6,23 @@ export default function AllStations(props) {
   const networkId = "bicing";
   const networkName = "Bicing";
 
+  // API Call for the list of stations and available bikes
+  // TODO: Error/failure handling
+
   useEffect(() => {
-    fetch(
-      "http://api.citybik.es/v2/networks/"+networkId
-    ).then(async (response) => {
-      if (response.ok) {
-        const temp = await response.json();
-        setStations(temp.network.stations)
-      }
-    });
+    fetch("http://api.citybik.es/v2/networks/" + networkId)
+      .then((response) => response.json())
+      .then((data) => setStations(data.network.stations));
   }, []);
 
-  let stationsList = <div>loading...</div>
+  let stationsList;
   if (stations) {
-    stationsList = stations.map((e) => <AllStationsItem key={e.id} name={e.name} />);
-  }
+    stationsList = stations.map((e) => <AllStationsItem key={e.id} data={e} />);
+  } else stationsList = <div>loading...</div>;
 
   return (
     <div className="bg-gray-light m-2 p-2">
-      <p className="font-bold text-xl">All Stations for...</p>
+      <p className="font-bold text-xl">All Stations for</p>
       <p className="text-xl">{networkName}</p>
       {stationsList}
     </div>
--- src/components/AllStationsItem.js
+++ src/components/AllStationsItem.js
@@ -1,10 +1,25 @@
 export default function AllStationsItem(props) {
-
   return (
     <div className="bg-white sm:flex sm:justify-between my-2 p-1">
-      <div><p className="font-bold">{props.name}</p>
-      <p>Station Info</p></div>
-      <p>Station Links</p>
+      <div>
+        <p className="font-extrabold">{props.data.name}</p>
+        <p>
+          <span className="font-semibold">Available Bikes: </span>
+          {props.data.extra.normal_bikes}
+        </p>
+        <p>
+          <span className="font-semibold">Available Ebikes: </span>
+          {props.data.extra.ebikes}
+        </p>
+        <p>
+          <span className="font-semibold">Return Slots: </span>
+          {props.data.empty_slots}
+        </p>
+      </div>
+      <div>
+        <p>Get Directions</p>
+        <p>Favorite This</p>
+      </div>
     </div>
   );
 }
