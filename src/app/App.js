bdcfb39d562e4da616c46b6016054004f631ea1c
Tobias Tobak <tobiasfunction@protonmail.com>
2022-08-12 18:41:56 -0400

    Add footer text

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,7 @@ import Networks from "../features/networks/Networks";
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="bg-blue text-white">
+      <header className="bg-blue text-white p-1">
         <img src={logo} alt="Where's A Bike?" className="mx-auto" />
       </header>
       {/* app body */}
@@ -21,7 +21,13 @@ function App() {
           <StationList />
         </div>
       </div>
-      <footer className="w-full bg-blue text-white self-end p-2">footer</footer>
+      <footer className="w-full bg-blue text-white self-end p-2">
+        <div className="m-2 ml-8">
+          <p className="text-xl font-black mb-1">Where's A Bike?</p>
+          <p>112 98th Street</p>
+          <p>Pittsburgh, PA 15212</p>
+        </div>
+      </footer>
     </div>
   );
 }
