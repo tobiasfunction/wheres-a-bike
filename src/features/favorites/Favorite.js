c164ebf9884dddb06d771d0d5ae264cf95cecfa3
Tobias Tobak <tobiasfunction@protonmail.com>
2022-08-12 19:43:17 -0400

    Added styling to match mock-up

--- src/features/favorites/Favorite.js
+++ src/features/favorites/Favorite.js
@@ -1,3 +1,10 @@
+import xIcon from "../../common/xIcon.svg";
+
 export default function Favorite(props) {
-  return <div />;
+  return (
+    <div className="flex flex-row">
+      <img src={xIcon} className="cursor-pointer w-3 m-1" />
+      <p className="font-bold text-sm m-1">{props.data.name}</p>
+    </div>
+  );
 }
--- src/features/favorites/FavoriteList.js
+++ src/features/favorites/FavoriteList.js
@@ -1,16 +1,20 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
+
+import Favorite from "./Favorite";
 
 export default function FavoriteList(props) {
-const favoritesData = useSelector((state) => state.favorite.list)
+  const favoritesData = useSelector((state) => state.favorite.list);
 
-
-
-const favorites = favoritesData.length ? favoritesData.map((e, i) => <div key={i}>{e.name}</div>) : <div>You don't have any favorite stations yet.</div>
+  const favorites = favoritesData.length ? (
+    favoritesData.map((e, i) => <Favorite key={i} data={e} />)
+  ) : (
+    <div>You don't have any favorite stations yet.</div>
+  );
 
   return (
-    <div className="bg-blue text-white border-2 border-gray-dark m-2 p-1">
-      <p className="font-extrabold text-lg text-center">Favorite Stations</p>
-      {favorites}
+    <div className="bg-blue text-white border-1 border-gray-dark m-2 p-2">
+      <p className="font-black text-xl text-center m-2">Favorite Stations</p>
+      <div className="overflow-y-auto h-80">{favorites}</div>
     </div>
   );
 }
