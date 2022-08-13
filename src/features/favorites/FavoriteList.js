0c16065a6b74729303f5d2b67990b32ab939e2f3
Tobias Tobak <tobiasfunction@protonmail.com>
2022-08-12 20:02:19 -0400

    Fix positioning for "no favorites" message

--- src/features/favorites/FavoriteList.js
+++ src/features/favorites/FavoriteList.js
@@ -8,12 +8,18 @@ export default function FavoriteList(props) {
   const favorites = favoritesData.length ? (
     favoritesData.map((e, i) => <Favorite key={i} data={e} />)
   ) : (
-    <div>You don't have any favorite stations yet.</div>
+    <div className="flex items-center h-full">
+      <p className="text-center m-4">
+        You don't have any favorite stations yet
+      </p>
+    </div>
   );
 
   return (
     <div className="bg-blue text-white border-1 border-gray-dark m-2 p-2">
-      <p className="font-black text-xl text-center m-2">Favorite Stations</p>
+      <div className="font-black text-xl text-center m-2">
+        Favorite Stations
+      </div>
       <div className="overflow-y-auto h-80">{favorites}</div>
     </div>
   );
